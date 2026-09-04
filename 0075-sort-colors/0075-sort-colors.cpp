37b22bd29388fd1089c1087b0f5b3f24c9ea1bd7
divyanshjajoo699 <divyanshjajoo699@gmail.com>
2026-09-04 10:40:23 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& arr) {
         int n=arr.size();
+        //m-1 bubble sort / any sorting algo 
         // for(int i=0;i<n-1;i++){
         //     for(int j=0;j<n-i-1;j++){
         //         if(arr[j]>arr[j+1]){
@@ -10,52 +11,48 @@ public:
         //     }
         // }
 
-    // long long  countzero,countone,counttwo;
-    //     for(long long i=0;i<n;i++){
-    //       if(arr[i]==0){
-    //          countzero++;
-    //       } 
-    //         if(arr[i]==1){
-    //         countone++;
-    //       }
-    //       else{
-    //         counttwo++;
-    //       }
-    //     }
+        //m-2 using count approach better approach O(2n)
+        // int countzero=0;
+        // int countone=0;
+        // int counttwo=0;
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==0){
+        //         countzero++;
+        //     }
+        //     else if(arr[i]==1){
+        //         countone++;
+        //     }
+        //     else if(arr[i]==2){
+        //         counttwo++;
+        //     }
+        // }
+        // // for(int i=0;i<countzero;i++){
+        // //     arr[i]=0;
+        // // }
+        // // for(int i=countzero;i<countzero+countone;i++){
+        // //     arr[i]=1;
+        // // }
+        // // for(int i=countzero+countone;i<countzero+countone+counttwo;i++){
+        // //     arr[i]=2;
+        // // }
 
-    //     for(long long i=0;i<countzero;i++){
-    //         arr[i]=0;
-    //     }
-    //     for(long long  i=countzero;i<countzero+countone;i++){
-    //         arr[i]=1;
-    //     }
-    //     for(long long i=countzero+countone;i<countzero+countone+counttwo;i++){
-    //         arr[i]=2;
-    //     }
-        int countzero=0;
-        int countone=0;
-        int counttwo=0;
+        //m-3 DNS O(n)
+        int low=0;int mid=0;int high=n-1;
+
         for(int i=0;i<n;i++){
-            if(arr[i]==0){
-                countzero++;
+            if(arr[mid]==0){
+                swap(arr[low],arr[mid]);
+                low++;mid++;
             }
-            else if(arr[i]==1){
-                countone++;
+            else if(arr[mid]==1){
+                mid++;
             }
-            else if(arr[i]==2){
-                counttwo++;
+            else{
+                swap(arr[mid],arr[high]);
+                high--;
             }
         }
-
-        for(int i=0;i<countzero;i++){
-            arr[i]=0;
-        }
-        for(int i=countzero;i<countzero+countone;i++){
-            arr[i]=1;
-        }
-        for(int i=countzero+countone;i<countzero+countone+counttwo;i++){
-            arr[i]=2;
-        }
+    
 
 
     }
