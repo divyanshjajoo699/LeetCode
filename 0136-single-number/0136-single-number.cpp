934c28ac3de876e51cec10d2781de8cd40ad8e7f
divyanshjajoo699 <divyanshjajoo699@gmail.com>
2026-09-09 21:49:30 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& arr) {
        int n=arr.size();
-       int xorr=0;int result=0;
+       int xorr=0;
        for(int i=0;i<n;i++){
-         result=result^arr[i];
+         xorr=xorr^arr[i];
        }
-       return result;
+       return xorr;
     }
 };
