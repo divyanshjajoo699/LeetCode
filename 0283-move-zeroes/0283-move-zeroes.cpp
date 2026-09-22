8154345fcb23324caf35924e38ac128830f8dab0
divyanshjajoo699 <divyanshjajoo699@gmail.com>
2026-09-22 23:32:17 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -21,8 +21,17 @@ public:
         //     }
         // }
 
+    // int n=arr.size();
+    //     int i=0;int j=1;
+    // for(int j=0;j<n;j++){
+    //     if(arr[j]!=0){
+    //         swap(arr[i],arr[j]);
+    //         i++;
+    //     }
+    // }
+
     int n=arr.size();
-        int i=0;int j=1;
+    int i=0;int j=1;
     for(int j=0;j<n;j++){
         if(arr[j]!=0){
             swap(arr[i],arr[j]);
@@ -31,3 +40,8 @@ public:
     }
     }
 };
+
+
+//brute approach is by one loop i am checking for zero elements and marking then with index j and in the second loop i am finding the elements !=0 and swapping with the element at index j
+
+//optimised we use 2 pointer approach in which we put i=0 and j=1 and iterate the j till end and check if arr[j]!=0 then swap it with the index i and then i++ so that we can assume next element to be zero 
